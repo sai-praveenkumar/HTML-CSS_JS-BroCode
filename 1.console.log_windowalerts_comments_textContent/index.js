c181b0ab3e763ab2ce680c5c20cffb030b01bf50
SaiPraveenKumar <saipraveenk816@gmail.com>
2024-05-17 17:36:19 +0530

    new file

---
+++ 1.console.log_windowalerts_comments_textContent/index.js
@@ -0,0 +1,14 @@
+console.log(`Hello`)
+
+//This is a single line comment
+console.log(`Hii`)
+
+/*This is 
+a  multi line 
+project
+*/ 
+window.alert("Time to work and study")
+window.alert(`Learn Javascript`)
+
+document.getElementById("H1").textContent = `Hello`;
+document.getElementById("P").textContent = "I like guava";
