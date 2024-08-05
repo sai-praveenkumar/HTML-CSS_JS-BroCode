9e3282c0ade70c81b7976f2f2852ffc788a7801f
SaiPraveenKumar <saipraveenk816@gmail.com>
2024-08-05 09:43:07 +0530

    new changes made

--- 5.Type_Conversions/type_conversion.js
+++ 5.Type_Conversions/type_conversion.js
@@ -1,4 +1,11 @@
-let age = window.prompt("How old are you ?");
-age=Number(age);
-age+=1;
-console.log(age);
+let x = "pizza";
+let y = "pizza";
+let z = "pizza";
+
+x=Number(x);
+y=String(y);
+z=Boolean(z);
+
+console.log(x,typeof x);
+console.log(y,typeof y);
+console.log(z,typeof z);
